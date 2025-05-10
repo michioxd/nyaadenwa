eacbd6717bf1f936c231e7c676a49406ac9bd6eb
michioxd <michio.haiyaku@gmail.com>
2025-05-10 09:20:42 +0700

    adding stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,51 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+let counter = 0;
+const nameMap = new Map<string, string>();
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css:
+    process.env.NODE_ENV === "production"
+      ? {
+          modules: {
+            generateScopedName: (
+              localName: string,
+              filename: string,
+            ): string => {
+              const key = `${filename}|${localName}`;
+              return (
+                nameMap.get(key) ??
+                nameMap
+                  .set(
+                    key,
+                    (() => {
+                      let name = "",
+                        n = counter++;
+                      do {
+                        name = [..."abcdefghijklmnopqrstuvwxyz"][n % 26] + name;
+                        n = Math.floor(n / 26) - 1;
+                      } while (n >= 0);
+                      return name;
+                    })(),
+                  )
+                  .get(key)!
+              );
+            },
+          },
+        }
+      : {},
+  build: {
+    minify: "terser",
+    terserOptions: {
+      parse: {
+        html5_comments: false,
+      },
+      format: {
+        comments: false,
+      },
+    },
+  },
+});
