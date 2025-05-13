f27545ad149be52ba933dcb69cf075c97f1b487f
michioxd <michio.haiyaku@gmail.com>
2025-05-13 09:51:35 +0700

    adding vite plugins

--- vite.config.ts
+++ vite.config.ts
@@ -7,13 +7,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
+import viteCompression from "vite-plugin-compression";
+// import { VitePWA } from 'vite-plugin-pwa' implement later
 
 let counter = 0;
 const nameMap = new Map<string, string>();
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        ViteMinifyPlugin({
+            ignoreCustomComments: []
+        }),
+        viteCompression(),
+    ],
     optimizeDeps: {
         exclude: ["@yume-chan/scrcpy-decoder-tinyh264"],
         include: ["@yume-chan/scrcpy-decoder-tinyh264 > yuv-buffer", "@yume-chan/scrcpy-decoder-tinyh264 > yuv-canvas"],
@@ -26,29 +35,29 @@ export default defineConfig({
     css:
         process.env.NODE_ENV === "production"
             ? {
-                  modules: {
-                      generateScopedName: (localName: string, filename: string): string => {
-                          const key = `${filename}|${localName}`;
-                          return (
-                              nameMap.get(key) ??
-                              nameMap
-                                  .set(
-                                      key,
-                                      (() => {
-                                          let name = "",
-                                              n = counter++;
-                                          do {
-                                              name = [..."abcdefghijklmnopqrstuvwxyz"][n % 26] + name;
-                                              n = Math.floor(n / 26) - 1;
-                                          } while (n >= 0);
-                                          return name;
-                                      })(),
-                                  )
-                                  .get(key)!
-                          );
-                      },
-                  },
-              }
+                modules: {
+                    generateScopedName: (localName: string, filename: string): string => {
+                        const key = `${filename}|${localName}`;
+                        return (
+                            nameMap.get(key) ??
+                            nameMap
+                                .set(
+                                    key,
+                                    (() => {
+                                        let name = "",
+                                            n = counter++;
+                                        do {
+                                            name = [..."abcdefghijklmnopqrstuvwxyz"][n % 26] + name;
+                                            n = Math.floor(n / 26) - 1;
+                                        } while (n >= 0);
+                                        return name;
+                                    })(),
+                                )
+                                .get(key)!
+                        );
+                    },
+                },
+            }
             : {},
     build: {
         target: ["es2022"],
